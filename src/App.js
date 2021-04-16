1b4457b519715784aba52b8e611e5b6bb3ffff63
Maya Srikanth <mayasrikm@Mayas-MacBook-Pro.local>
2021-04-15 18:06:46 -0700

    updated to work with backend

--- src/App.js
+++ src/App.js
@@ -3,31 +3,92 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Input from './components/Input';
 import Source from './components/Source';
-import {useState}  from 'react';
+import {useState, useEffect}  from 'react';
 function App(){
   
-  const [sources, setSources] = useState([
-      {"id": 1, "name" :"Presbo" ,"email":"presbo@columbia.edu"},
-      {"id": 2, "name":"John Jay Mouse" ,"email":"mouse@columbia.edu"},
-      {"id": 3, "name":"Water Bottle Man" ,"email":"flipper@columbia.edu"}
-  ]);
+  const [sources, setSources] = useState([]);
+  useEffect(()=> fetchData());
+
+  const fetchData = async () => {
+    fetch(`/api/sources`, {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json'
+      }})
+      .then(response => {
+          if (response.status >= 200 && response.status < 300) {
+              return response;
+            }
+            const error = new Error(`HTTP Error ${response.statusText}`);
+            error.status = response.statusText;
+            error.response = response;
+            console.log(error);
+            throw error;
+      })
+      .then(response => response.json())
+      .then(json => {
+          console.log(json);
+          setSources(json.data);
+      })
+      .catch(error => console.log(error));
+  };
+  
   let new_id = sources.length+1;
+
   const deleteSource = (id)=>{
-    const newSources = sources.filter((source)=>source.id!==id);
-    newSources.forEach((source) =>{
-      if (source.id>id){  
-        source.id-=1;}
-    })
-    setSources(newSources);
+    fetch(`/api/delete_source/${id}`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      }})
+      .then(response => {
+          if (response.status >= 200 && response.status < 300) {
+              return response;
+            }
+            const error = new Error(`HTTP Error ${response.statusText}`);
+            error.status = response.statusText;
+            error.response = response;
+            console.log(error);
+            throw error;
+      })
+      .then(response => response.json())
+      .then(json => {
+          console.log(json);
+          setSources([json.data]);
+      })
+      .catch(error => console.log(error));
   };
   const submitSource = (name,email)=> {
     if (name!==""){
       const newSource = {
         "id":new_id, "name": name,"email":email
       };
-    new_id +=1;
-    setSources([...sources,newSource]);}
-  }
+      new_id +=1;
+      fetch(`/api/add_source`, {
+        method: 'POST',
+        body: JSON.stringify({source: newSource}),
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+        .then(response => {
+            if (response.status >= 200 && response.status < 300) {
+                return response;
+              }
+              const error = new Error(`HTTP Error ${response.statusText}`);
+              error.status = response.statusText;
+              error.response = response;
+              console.log(error);
+              throw error;
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            setSources([json.data]);
+        })
+        .catch(error => console.log(error));
+    
+    }
+  };
     return (
         <div>
             <NavBar />
@@ -37,14 +98,10 @@ function App(){
                   return (
                       <Source source = {source} deleteSource={deleteSource}/>
                     )
-              })}
-              
-                
-              
-            
+              })} 
           </div> 
     );
-  }
+}
 
 
 
